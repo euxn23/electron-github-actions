77f4438963d3a552e53ea9a6e8c9e0339dce1cf7
euxn23 <euxn23@gmail.com>
2019-09-23 23:42:20 +0900

    feat: add github actions

--- src/main.js
+++ src/main.js
@@ -12,12 +12,13 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      webSecurity: false
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, 'index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
